d2d65447b4f3cc4b4937a5511d2d97cd17c63e55
ibrahim-khalil <ibrahimkhalil@users.noreply.github.com>
2020-10-08 03:13:24 +0500

    commit

---
+++ precache-manifest.3dcec866d1f83992168898426f7948b1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2330ac4edcf547e7615b4a6b7872f59f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d468e21994ebc651f021",
+    "url": "/static/css/main.8d7cf30a.chunk.css"
+  },
+  {
+    "revision": "71cb4bad829785da214f",
+    "url": "/static/js/2.e0cdfcab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e0cdfcab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d468e21994ebc651f021",
+    "url": "/static/js/main.ec6a42e8.chunk.js"
+  },
+  {
+    "revision": "d8d3eca2d5da98c3bc91",
+    "url": "/static/js/runtime-main.9fff5107.js"
+  },
+  {
+    "revision": "8cec250e96fe3ef0202f8115640c1a86",
+    "url": "/static/media/Scattered Clouds.8cec250e.png"
+  },
+  {
+    "revision": "c67755ac6475a06e3075bb6c91dccd55",
+    "url": "/static/media/Smoke.c67755ac.jpg"
+  },
+  {
+    "revision": "52662f24ae8da51ecc58f4358b2399fc",
+    "url": "/static/media/city scape.52662f24.png"
+  },
+  {
+    "revision": "410560155f7cad20c51c2d5dd829544b",
+    "url": "/static/media/ibrahim.41056015.jpg"
+  },
+  {
+    "revision": "95d72d5ca4d4fab9fa7b7ec512cdf8c1",
+    "url": "/static/media/question.95d72d5c.png"
+  }
+]);
